9d0cab5554bde0c329f09be7477353e159f39135
= <=>
2026-01-13 02:57:02 +0200

    f

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,19 @@ use Inertia\Inertia;
 |--------------------------------------------------------------------------
 */
 
+// .well-known routes for deep linking (Android/iOS)
+Route::get('/.well-known/assetlinks.json', function () {
+    return response()->file(public_path('.well-known/assetlinks.json'), [
+        'Content-Type' => 'application/json',
+    ]);
+});
+
+Route::get('/.well-known/apple-app-site-association', function () {
+    return response()->file(public_path('.well-known/apple-app-site-association'), [
+        'Content-Type' => 'application/json',
+    ]);
+});
+
 Route::get('/', function () {
     if (Auth::check()) {
         return redirect()->route('dashboard');
